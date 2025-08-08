22be17da100cd6e1518b7d4e8229c7b49d51a1dc
Mustafa Bayrak <bayrakmusti@gmail.com>
2025-08-08 12:05:42 +0300

    restructured the repo so it automatically builds the multivessel_msgs

--- multiple_vessels/nodes/multiple_vessel_node_starter.py
+++ multiple_vessels/nodes/multiple_vessel_node_starter.py
@@ -1,104 +1,122 @@
 #!/usr/bin/env python
-"""
-This node is the Global_Path_planner node
-for every spawned Vessel in the sim environment.
-
-In this ROS node, Vessel_global_planner class is defined for every Vessel that's available in the 
-Gazebo simulation environment.
-
-Global Motion Planner Node that generates global waypoints 
-"""
-
-from os import kill
+from __future__ import print_function
 import rospy
-from rospy.core import rospydebug
-import json
-import os
-import time
+import json, os, time
 
-class Vessel_Node_Starter():
-    def __init__(self,vessel_count_):
+class Vessel_Node_Starter(object):
+    def __init__(self, vessel_count_):
         self.path = "~/vrx_ws/src/vrx/MultiVessel_Simulation/multiple_vessels"
         self.full_path = os.path.expanduser(self.path)
-        #One object of this class will be called for each vessel.
 
-        with open(self.full_path+'/json_files/config.json','r') as f:    
-            self.config_data = json.load(f)
+        with open(os.path.join(self.full_path, 'json_files', 'config.json'), 'r') as f:
+            self.cfg = json.load(f)
+
         self.vessel_count = vessel_count_
-        self.Start_Nodes()
+        self._compute_choices()
+        self._start_nodes()
 
-    def Start_Nodes(self):
-        #This function sets the ROS parameters for every vessel:
-        #https://linuxcommandlibrary.com/man/gnome-terminal
-        #https://www.baeldung.com/linux/gnome-terminal-command-line
+    # -------- helpers --------
+    def _g(self, *keys, default=False):
+        """
+        Safe getter for nested dict/list with default.
+        Usage: self._g("controller", 0, "Pure_Pursuit", 0, "chosen", default=False)
+        """
+        cur = self.cfg
+        try:
+            for k in keys:
+                cur = cur[k]
+            return cur
+        except (KeyError, IndexError, TypeError):
+            return default
 
+    def _term(self, cmd):
+        os.system('gnome-terminal --tab -- bash -c "source ~/vrx_ws/devel/setup.bash; %s; bash"' % cmd)
 
-        #If the global path planner is chosen, start the ROS node in a new tab.
-        self.global_path_planner_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Global_Path_Planner"]
+    def _compute_choices(self):
+        # --- Local path planning
+        self.rrt_chosen = self._g("local_path_planning", 0, "RRT", 0, "chosen", default=False)
 
-        if self.global_path_planner_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_global_path_planner_node.py; bash\" ")
+        # --- Controller choices
+        self.pp_chosen      = self._g("controller", 0, "Pure_Pursuit",   0, "chosen", default=False)
+        self.pid_chosen     = self._g("controller", 0, "PID_Controller", 0, "chosen", default=False)
+        self.stanley_chosen = self._g("controller", 0, "Stanley",        0, "chosen", default=False)
 
-        time.sleep(0.5)
-        #If the perception is chosen, start the ROS node in a new tab.
-        self.perception_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Perception"]
+        # --- Ship domain
+        self.ship_domain_chosen = self._g("ship_domain", 0, "Circular_Ship_Domain", 0, "chosen", default=False)
 
-        if self.perception_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python perception_pose_aggregator_node.py; bash\" ")
+        # --- Perception / localization (tie to Subsribe_to_Topics -> choosen)
+        self.perception_chosen = self._g("Localization", 0, "Subsribe_to_Topics", 0, "choosen", default=False)
 
-        time.sleep(0.5)
-        #If the ship domain is chosen, start the ROS node in a new tab.
-        self.ship_domain_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Ship_Domain"]
+        # --- Loggers
+        self.sim_logger_on = (
+            self._g("simulation_logger", 0, "logging_available",  default=False) or
+            self._g("simulation_logger", 0, "plotting_available", default=False)
+        )
+        self.local_path_plotter_logger_on = (
+            self._g("local_path_plotter_logger", 0, "logging_available",  default=False) or
+            self._g("local_path_plotter_logger", 0, "plotting_available", default=False)
+        )
 
-        if self.ship_domain_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_ship_domain_node.py; bash\" ")
+        # --- Items not represented directly in JSON: allow ROS params
+        # Default on for global path planner (common), off for trajectory tracker unless a non-PP controller is chosen,
+        # on for switch mechanism if you have both global & local behaviors.
+        self.global_path_planner_on = rospy.get_param("~enable_global_path_planner", True)
+        self.switch_mechanism_on    = rospy.get_param("~enable_switch_mechanism", True)
+        self.trajectory_tracker_on  = rospy.get_param("~enable_trajectory_tracker", True)
 
-        time.sleep(0.5)
-        #If the switch mechanism is chosen, start the ROS node in a new tab.
-        self.switch_mechanism_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Switch_Mechanism"]
+    def _start_nodes(self):
+        # Global Path Planner
+        if self.global_path_planner_on:
+            self._term("python3 multiple_vessel_global_path_planner_node.py")
+            time.sleep(0.4)
 
-        if self.switch_mechanism_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_switch_mechanism.py; bash\" ")
+        # Perception (from Localization->Subscribe_to_Topics->choosen)
+        if self.perception_chosen:
+            self._term("python3 perception_pose_aggregator_node.py")
+            time.sleep(0.4)
 
-        time.sleep(0.5)
-        #If the local path planner is chosen, start the ROS node in a new tab.
-        self.local_path_planner_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Local_Path_Planner"]
+        # Ship Domain
+        if self.ship_domain_chosen:
+            self._term("python3 multiple_vessel_ship_domain_node.py")
+            time.sleep(0.4)
 
-        if self.local_path_planner_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_local_path_planner_RRT.py; bash\" ")
+        # Switch Mechanism (gate between global/local)
+        if self.switch_mechanism_on:
+            self._term("python3 multiple_vessel_switch_mechanism.py")
+            time.sleep(0.4)
 
-        time.sleep(0.5)
-        #If the trajectory tracker is chosen, start the ROS node in a new tab.
-        self.trajectory_tracker_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Trajectory_Tracker"]
+        # Local Path Planner (RRT)
+        if self.rrt_chosen:
+            self._term("python3 multiple_vessel_local_path_planner_RRT.py")
+            time.sleep(0.4)
 
-        if self.trajectory_tracker_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_trajectory_tracker.py; bash\" ")
+        # Trajectory Tracker (use if PID/Stanley etc.)
+        if self.trajectory_tracker_on:
+            self._term("python3 multiple_vessel_trajectory_tracker.py")
+            time.sleep(0.4)
 
-        time.sleep(0.5)
-        #If the pure pursuit is chosen, start the ROS node in a new tab.
-        self.pure_pursuit_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Pure_Pursuit_Controller"]
+        # Pure Pursuit Controller (mutually exclusive with traj. tracker in most stacks)
+        if self.pp_chosen:
+            self._term("python3 multiple_vessel_controller_purePursuit.py")
+            time.sleep(0.4)
 
-        if self.pure_pursuit_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python multiple_vessel_controller_purePursuit.py; bash\" ")
+        # Simulation logger
+        if self.sim_logger_on:
+            self._term("python3 simulation_logger_node.py")
+            time.sleep(0.4)
 
-        time.sleep(0.5)
-        #If the logger node is chosen, start the ROS node in a new tab.
-        self.logger_chosen = self.config_data["simulation"][0]["available_nodes"][0]["Logger"]
-
-        if self.logger_chosen == True:
-            os.system("gnome-terminal --tab -- bash -c \"source ~/vrx_ws/devel/setup.bash;python simulation_logger_node.py; bash\" ")
-
-
-        
-
-        pass
+        # Local-path plotter/logger (optional, if you have a separate node/file for it)
+        if self.local_path_plotter_logger_on:
+            # Change to your actual script name if different:
+            # self._term("python local_path_plotter_logger_node.py")
+            pass
 
 if __name__ == '__main__':
-    rospy.init_node('multi_vessel_global_planner', anonymous=True)
-    hz = 10
-    rate = rospy.Rate(hz)
+    rospy.init_node('multi_vessel_starter', anonymous=True)
+    rate = rospy.Rate(10)
 
-    vessel_count = int(rospy.get_param("/vessel_count"))#get the number of how many vessels are spawned in this run
-    print("Number of Vessels: ", str(vessel_count))
+    vessel_count = int(rospy.get_param("/vessel_count", 1))
+    print("Number of Vessels:", vessel_count)
 
     Vessel_Node_Starter(vessel_count)
+    rospy.spin()
